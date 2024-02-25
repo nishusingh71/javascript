554db19cba2d1b1a88f0463b98aaa8bff6158089
Nishu Singh <53341807+nishusingh71@users.noreply.github.com>
2024-02-25 06:30:13 +0000

    updates

---
+++ Variables/variable.js
@@ -0,0 +1,19 @@
+// console.log("Hello World");
+
+//var 
+var firstName="Nishu"
+console.log(firstName);
+firstName="Rohit"
+console.log(firstName)
+
+//let variable
+let number=1;
+// number++;
+console.log(number);
+number=5;
+console.log(number);
+
+//const variable
+const PI=3.14;
+let area=PI*3*3;
+console.log(area);
