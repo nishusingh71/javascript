d8e3918449bca788d6e020931a339986d8b891c5
Nishu Singh <53341807+nishusingh71@users.noreply.github.com>
2024-03-02 15:24:52 +0000

    updates

--- lowerToUpperCase.js
+++ lowerToUpperCase.js
@@ -1,39 +1,19 @@
-// let str = "hello world";
-// let str1 = "";
-// let ch = str.charAt(0).toUpperCase();
-// str1 += ch;
-// for (let i = 1; i < str.length; i++) {
-//     if (str.charAt(i) === '' && i < str.length - 1) {
-//         str1 += str.charAt(i);
-//         i++;
-//         str += str.charAt(i).toUpperCase();
-//     } else {
-//         str1 += str.charAt(i);
-//     }
-// }
-// console.log(str1);
-
-class UpperCase {
-    static toUpperCase(str) {
-        let sb = "";
-        let ch = str.charAt(0).toUpperCase();
-        sb += ch;
-        for (let i = 1; i < str.length; i++) {
-            if (str.charAt(i) === ' ' && i < str.length - 1) {
-                sb += str.charAt(i);
-                i++;
-                sb += str.charAt(i).toUpperCase();
-            } else {
-                sb += str.charAt(i);
-            }
+// Lower case to upper case 
+let str = "NISHU";
+let res = "";
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let capitalizied=true;
+for (let i = 0; i <= str.length - 1; i++) {
+    let char = str[i];
+    for (let j = 0; j <= lowerCase.length - 1; j++) {
+        if (char === lowerCase[j]) {
+            res += upperCase[j];
+        } else if (char === upperCase[j]) {
+            res += lowerCase[j];
         }
-        return sb;
-    }
-    static main(args) {
-        let str = "hello world";
-        console.log(UpperCase.toUpperCase(str));
     }
 }
+console.log(res);
 
-UpperCase.main();
-
+//Capitalized First Letter.
