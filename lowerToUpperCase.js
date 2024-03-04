a17b17246ea4f60fe7837514cc1e1d59399e8608
Nishu Singh <53341807+nishusingh71@users.noreply.github.com>
2024-03-04 14:19:53 +0530

    Update lowerToUpperCase.js

--- lowerToUpperCase.js
+++ lowerToUpperCase.js
@@ -1,15 +1,15 @@
 let str = String(prompt("Enter a text"));
 let result = "";
 for (let i = 0; i < str.length; i++) {
-    let currentChar = str.currentCharAt(i);
+    let currentChar = str.charAt(i);
     //lower case to upper case
     if (currentChar >= 'a' && currentChar <= 'z') {
-        let upperCase = String.fromCharCode(currentChar.currentCharCodeAt(0) - 32);
+        let upperCase = String.fromCharCode(currentChar.charCodeAt(0) - 32);
         result += upperCase;
     }
     //upper case  to lower case
     else if (currentChar >= 'A' && currentChar <= 'Z') {
-        let lowerCase = String.fromCharCode(currentChar.currentCharCodeAt(0) + 32);
+        let lowerCase = String.fromCharCode(currentChar.charCodeAt(0) + 32);
         result += lowerCase;
     } else {
 
